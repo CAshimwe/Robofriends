c21b8f77e33755dcc3c2e452309c4d125fa90d4f
Constantin ASHIMWE <63354842+CAshimwe@users.noreply.github.com>
2020-05-08 15:59:59 +0200

    Updates

---
+++ precache-manifest.040a312fad5435dd320deec1ac64c78a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f658934e911c941de5c636d862bae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "8da50ec87deb7b7d753c",
+    "url": "/static/css/main.871632b6.chunk.css"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/static/js/2.2b73e027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b73e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da50ec87deb7b7d753c",
+    "url": "/static/js/main.ad0fcbcb.chunk.js"
+  },
+  {
+    "revision": "46b2ab5944883490d30d",
+    "url": "/static/js/runtime-main.501fdd23.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
